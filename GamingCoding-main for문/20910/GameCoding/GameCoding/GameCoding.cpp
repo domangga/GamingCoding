59967bfcd537d9e1478141576807a6e806266655
도멩가 <121483261+domangga@users.noreply.github.com>
2023-11-28 17:07:33 +0900

    Add files via upload

---
+++ GamingCoding-main for문/20910/GameCoding/GameCoding/GameCoding.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+// [타입][이름];
+
+//char :1
+//short :2
+//int :4
+// __int64(long long) :8
+
+int cnt;
+
+
+int main()
+{
+    cnt = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Hello World" << endl;
+
+        if (i == 50)
+            continue;
+    }
+}
+
+
